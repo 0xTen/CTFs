29eed9f74a1f00ac4b35758fcfdd557c76c21600
0xTen <pedroguerrap2002@gmail.com>
2021-11-22 16:21:42 -0300

    Add n1ctf/babyguess

---
+++ n1ctf/2021/babyguess/boot.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+qemu-system-x86_64  -m 512M  -kernel ./bzImage   -initrd  ./rootfs.img --append "loglevel=3 console=ttyS0 root=/dev/ram init=/init kaslr" -smp cores=2,threads=4 -cpu kvm64,+smep,+smap -monitor /dev/null -s -nographic
---
+++ n1ctf/2021/babyguess/compress.sh
@@ -0,0 +1,7 @@
+gcc -o exploit -static $1 -lpthread
+mv ./exploit ./rootfs
+cd rootfs
+find . -print0 \
+| cpio --null -ov --format=newc \
+ > rootfs.img
+mv ./rootfs.img ../
